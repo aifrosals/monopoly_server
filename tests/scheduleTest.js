77648f3b73faf80e597a572fde88e18de0a29357
aifrosals <a.ifrosals@gmail.com>
2022-03-04 17:48:45 +0500

    production

--- tests/scheduleTest.js
+++ tests/scheduleTest.js
@@ -1,6 +1,21 @@
 const schedule = require('node-schedule')
+const sinon = require('sinon')
 
 
 const job = schedule.scheduleJob('* * * * *', function() {
     console.log('job working one minute')
 })
+
+function testSchedule() {
+    var clock = sinon.useFakeTimers()
+    console.log('testSchedule')
+    console.log('clock.now', clock.now)
+    console.log('Date', new Date())
+    clock.tick(600000)
+    console.log('clock.now', clock.now)
+    console.log('Date', new Date())
+
+}
+
+testSchedule()
+
