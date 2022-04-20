99fa1e6849083cc472dcb97126b6a7dc49c3ed59
aifrosals <a.ifrosals@gmail.com>
2022-04-20 14:11:38 +0500

    server

---
+++ config.js
@@ -0,0 +1,7 @@
+ const productionDB = 'mongodb+srv://monopoly:monopoly1122@cluster0.m2chq.mongodb.net/monopoly_db?retryWrites=true&w=majority'
+ const devDB = 'mongodb+srv://monopoly:monopoly1122@cluster0.m2chq.mongodb.net/monopoly_db_local?retryWrites=true&w=majority'
+
+module.exports = {
+    productionDB: productionDB,
+    devDB: devDB
+}
